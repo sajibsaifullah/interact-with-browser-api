5008bf0e4849ab65ccb767e1c0153c79844d2a5a
sajibsaifullah <dusajibphy@gmail.com>
2023-03-10 16:00:05 +0600

    first commit

---
+++ browser-api-notes.js
@@ -0,0 +1,14 @@
+/* 
+==> to change test of any site
+        document.body.contentEditable = True
+*/
+
+/* 
+    ==== Local Storage ====
+
+localStorage.setItem('key', 'value')
+localStorage.getItem('key')
+localStorage.removeItem('key')
+
+
+*/
